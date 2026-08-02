e1b9a0d6459dbc358fcc5b695cc4f6f98be97296
Sandro <sandrocoelho2023a@gmail.com>
2026-08-02 17:13:33 -0300

    fix: resolve cors

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ connect();
 
 app.use(express.json());
 
-app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+app.use(cors({ credentials: true, origin: "http://localhost:3000", "https://minhas-receitas-rouge.vercel.app" }));
 
 app.use(
     "/uploads",
