83a8b40a1f040c0297f3dd61c1f281eeac9eb0c8
Sandro <sandrocoelho2023a@gmail.com>
2026-08-03 21:55:08 -0300

    fix: resolve curtidas.js 14

--- frontend/src/components/Curtidas.js
+++ frontend/src/components/Curtidas.js
@@ -74,7 +74,7 @@ const Curtidas = ({ idReceita }) => {
             }
 
             <div>
-                <p>{curtidas.count}</p>
+                <p>{curtidas.length}</p>
             </div>
         </div>
     )
