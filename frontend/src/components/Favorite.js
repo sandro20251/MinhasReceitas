35e89725056f75e4198e00f1c9a7fe8fb8ef54ba
Sandro <sandrocoelho2023a@gmail.com>
2026-08-05 11:47:25 -0300

    fix: resolve favorite 4

--- frontend/src/components/Favorite.js
+++ frontend/src/components/Favorite.js
@@ -5,9 +5,9 @@ import '../components/Favorite.css';
 // componente para favoritar/desfavoritar receita
 
 const Favorite = ({ idReceita }) => {
-    const [setBtnAtivo] = useState(false);
-    const [setMessage] = useState("");
-    const [setLoading] = useState(false);
+    const [,setBtnAtivo] = useState(false);
+    const [,setMessage] = useState("");
+    const [,setLoading] = useState(false);
     const { addFavorite, removeFavorite } = useRecipes();
     const [estadoFavorito, setEstadoFavorito] = useState(false);
 
